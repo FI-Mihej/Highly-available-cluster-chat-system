3243cea97d9663c59d9238b304c39a3c1c82a413
ButenkoMS <gtalk@butenkoms.space>
2016-08-03 22:19:25 +0300

    Made loader for servers list from txt file (each line holds single server address)

--- server_list_loader.py
+++ server_list_loader.py
@@ -6,3 +6,14 @@ Docstrings: http://www.python.org/dev/peps/pep-0257/
 __author__ = 'ButenkoMS <gtalk@butenkoms.space>'
 
 
+FILE_NAME = 'server_list.txt'
+
+
+def load_server_list(file_name=FILE_NAME):
+    result_list = list()
+    with open(file_name, 'rb') as file:
+        data = file.read()
+        data_lines = data.split(b'\n')
+        for line in data_lines:
+            result_list.append(line.strip())
+    return result_list
